1039a010d2370286437b8004580af4bac83cabe4
Said Meza <176p0393@itspozarica.edu.mx>
2025-12-27 00:13:15 -0600

    added funtion to detecte scroll and show the button

--- main.js
+++ main.js
@@ -4,3 +4,12 @@ function hiddenmenu() {
 
 }
 
+const btn = document.querySelector('.figure_arrows');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 200) {
+    btn.classList.add('show');
+  } else {
+    btn.classList.remove('show');
+  }
+});
